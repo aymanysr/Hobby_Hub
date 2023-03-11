4d60241b63d156e15287a4e4c5d68171300ee875
Afafe Slimani <slimani.afaf.iaa@gmail.com>
2023-03-11 18:20:27 +0100

    added some validations in group and user models

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user
   belongs_to :city
   belongs_to :category
-  has_many :users, through: :user_group
+  has_many :user_groups
+  has_many :users, through: :user_groups
   has_many :posts, dependent: :destroy
   has_one_attached :photo
 
