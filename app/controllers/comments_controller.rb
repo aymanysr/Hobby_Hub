55d96623d0b14c8d414640e5dd2663f72a89cbae
Aimane Yousr <35980026+aymanysr@users.noreply.github.com>
2025-08-12 13:47:05 +0100

    Handle comment validation failures

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,15 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.post_id = params["post_id"]
     @post = Post.find(params["post_id"])
-    if @comment.save!
+    if @comment.save
       respond_to do |format|
         format.html { redirect_to post_path(@comment.post_id), notice: 'comment was successfully created.' }
         format.text { render partial: "posts/comments", locals: { post: @post }, formats: [:html] }
       end
 
     else
+      Rails.logger.error("Failed to save comment: #{@comment.errors.full_messages.to_sentence}")
+      flash.now[:alert] = @comment.errors.full_messages.to_sentence
       render :new
     end
   end
